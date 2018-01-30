822a0cade656552ac2e4ba61cd78c793f5fea091
Josh Di Fabio <joshdifabio@gmail.com>
2018-01-30 16:40:01 +0000

    Bypass slow ProductRepository for SKU resolution

---
+++ Model/SkuResolver.php
@@ -0,0 +1,35 @@
+<?php
+namespace SnowIO\ExtendedProductRepository\Model;
+
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\DB\Adapter\AdapterInterface;
+
+class SkuResolver
+{
+    private $resourceConnection;
+    private $dbAdapter;
+
+    public function __construct(ResourceConnection $resourceConnection, AdapterInterface $dbAdapter = null)
+    {
+        $this->resourceConnection = $resourceConnection;
+        $this->dbAdapter = $dbAdapter ?? $resourceConnection->getConnection();
+    }
+
+    public function getProductIds(array $skus): array
+    {
+        if (empty($skus)) {
+            return [];
+        }
+
+        $select = $this->dbAdapter->select()
+            ->from($this->getProductTableName(), ['sku', 'entity_id'])
+            ->where('sku in (?)', $skus);
+
+        return $this->dbAdapter->fetchPairs($select);
+    }
+
+    private function getProductTableName()
+    {
+        return $this->resourceConnection->getTableName('catalog_product_entity');
+    }
+}
