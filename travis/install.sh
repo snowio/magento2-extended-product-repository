ce12be41cccc32eaa5eae5f95b60e5102ff9e404
Cristian Quiroz <cris@qrz.io>
2021-03-10 10:46:14 +0000

    Ignore platform reqs for composer install
    php 7.4 in travis is missing a few extensions. this is just a workaround for it
    see: https://travis-ci.community/t/gd-library-extension-not-available-with-php-7-4/6324

--- travis/install.sh
+++ travis/install.sh
@@ -4,7 +4,7 @@ PACKAGE_NAME=`composer config name`
 cd $HOME/build
 git clone --branch $MAGENTO_VERSION https://github.com/magento/magento2.git magento2ce --depth=1
 cd magento2ce
-composer install
+composer install --ignore-platform-reqs
 composer config minimum-stability dev
 composer require $PACKAGE_NAME:* --no-update
 composer update $PACKAGE_NAME
